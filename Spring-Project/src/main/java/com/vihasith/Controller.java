b7fdb7a66674d3ce38051e82f0208d0d0897d8af
siri8p <siri8p@gmail.com>
2025-01-15 21:35:29 -0800

    cmd commit

---
+++ Spring-Project/src/main/java/com/vihasith/Controller.java
@@ -0,0 +1,12 @@
+package com.vihasith;
+
+public class Controller {
+	int a=3;
+	
+	public static void main(String[] args) {
+	 Controller c = new Controller();
+	 System.out.println(c.a);
+		
+	}
+
+}
--- Spring-Project/src/main/java/com/vihasith/Entity.java
+++ Spring-Project/src/main/java/com/vihasith/Entity.java
@@ -2,5 +2,7 @@ package com.vihasith;
 
 public class Entity {
 
-
+    public static void main(String[] args) {
+		System.out.println("add the file");
+	}
 }
